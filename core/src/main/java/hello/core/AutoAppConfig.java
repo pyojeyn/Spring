ef694b58f72dad23d423d91dae26bb10c965ee2c
pyojeyn <jeynpyo@gmail.com>
2022-03-07 09:08:07 +0900

    Filter

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -5,9 +5,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-// ComponentScan : 이름 그대로 @Component 애노테이션이 붙은 클래스를 스캔해서 스프링 빈으로 등록한다. @Component를 붙여주자!
+// ComponentScan : 이름 그대로 @Component 애노테이션이 붙은 클래스를 스캔해서 스프링 빈으로 등록한다. @Component 를 붙여주자!
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes=Configuration.class)
 )
 public class AutoAppConfig {
