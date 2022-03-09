4488aae88dce9106c5f5268cd3a58f3a316ab75d
pyojeyn <jeynpyo@gmail.com>
2022-03-09 15:14:29 +0900

    Component 0309

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,5 +15,23 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes=Configuration.class)
 )
 public class AutoAppConfig {
-
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 }
+// 수동이 우선순위이다 @Bean > @Component !! MemberRepository에 @Component로 등록이 되어있는 상태인데 또 여기서 MemberRepository를 @Bean으로 등록해주었다.
+// 그러면 충돌이 발생한다. CoreApplication을 실행해보면 아래와 같은 오류가 발생할 것이다.
+/*
+***************************
+APPLICATION FAILED TO START
+***************************
+
+Description:
+
+The bean 'memoryMemberRepository', defined in class path resource [hello/core/AutoAppConfig.class], could not be registered. A bean with that name has already been defined in file [/Users/jennyboo/Documents/Spring/core/out/production/classes/hello/core/member/MemoryMemberRepository.class] and overriding is disabled.
+
+Action:
+
+Consider renaming one of the beans or enabling overriding by setting spring.main.allow-bean-definition-overriding=true
+*/
