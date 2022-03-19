f7336c962d77c6b400e7d050b16317c72fb8cd4a
pyojeyn <jeynpyo@gmail.com>
2022-03-19 10:58:42 +0900

    뭐지

--- again/src/test/java/hello/again/beanfind/ApplicationContextSameBeanFindTest.java
+++ again/src/test/java/hello/again/beanfind/ApplicationContextSameBeanFindTest.java
@@ -36,6 +36,7 @@ public class ApplicationContextSameBeanFindTest {
                 MemberRepository.class);
         assertThat(memberRepository).isInstanceOf(MemberRepository.class);
     }
+    //음
 
     @Test
     @DisplayName("특정 타입을 모두 조회하기")
