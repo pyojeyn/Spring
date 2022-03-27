90324d7369b7c5a3c112fe16b75f30896ebc5156
pyojeyn <jeynpyo@gmail.com>
2022-03-27 09:57:08 +0900

    0327 Provider

--- again/src/test/java/hello/again/scope/SingletonWithPrototypeTest1.java
+++ again/src/test/java/hello/again/scope/SingletonWithPrototypeTest1.java
@@ -3,12 +3,14 @@ package hello.again.scope;
 import org.assertj.core.api.Assert;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -38,20 +40,20 @@ public class SingletonWithPrototypeTest1 {
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
 
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성시점에 주입
+       // private final PrototypeBean prototypeBean; // 생성시점에 주입
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
@@ -73,7 +75,6 @@ public class SingletonWithPrototypeTest1 {
         @PostConstruct
         public void init(){
             System.out.println("PrototypeBean.init " + this);
-
         }
 
         @PreDestroy
