5e3060633352638e3b557f604a935286520f1521
pyojeyn <jeynpyo@gmail.com>
2022-03-29 09:47:20 +0900

    0329 log

--- again/src/main/java/hello/again/common/MyLogger.java
+++ again/src/main/java/hello/again/common/MyLogger.java
@@ -32,3 +32,12 @@ public class MyLogger {
         System.out.println("[" + uuid + "] request scope bean close:" + this);
     }
 }
+/*
+    로그를 출력하기 위한 'MyLogger 클래스이다.'
+    @Scope(value = "request") 를 사용해서 request 스코프로 지정했다 . 이게 이 빈은 HTTP 요청 당 하나씩 생성되고,
+    HTTP 요청이 끝나는 시점에 소멸된다.
+    이 빈이 생성되는 시점에 자동으로 @PostConstruct 초기화 메서드를 사용해서 uuid 를 생성해서 저장해둔다. 이 빈은 HTTP 요청 당 하나씩 생성되므로 ,
+    uuid 를 저장해두면 다른 HTTP 요청과 구분할 수 있다.
+    이 빈이 소멸되는 시점에 '@PreDestroy' 를 사용해서 종료 메시지를 남긴다.
+    'requestURL' 은 이 빈이 생성되는 시점에는 알 수 없으므로, 외부에서 setter 로 입력 받는다.
+*/
