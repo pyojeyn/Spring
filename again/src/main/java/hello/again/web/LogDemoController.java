88a742197087f6bdbc8b77c24fd1028e04a716fc
pyojeyn <jeynpyo@gmail.com>
2022-03-31 08:58:51 +0900

    0331 스코프와 Proxy

--- again/src/main/java/hello/again/web/LogDemoController.java
+++ again/src/main/java/hello/again/web/LogDemoController.java
@@ -21,6 +21,8 @@ public class LogDemoController {
     public String logDemo(HttpServletRequest request){
         MyLogger myLogger = myLoggerProvider.getObject();
         String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
