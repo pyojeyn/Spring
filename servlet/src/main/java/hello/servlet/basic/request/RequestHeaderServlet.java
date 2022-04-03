220fd0943ee9b037037cefdeec320444129a0df3
pyojeyn <jeynpyo@gmail.com>
2022-04-04 08:55:15 +0900

    0404 HTTP 요청 - GET 쿼리파라미터

--- servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -96,3 +96,22 @@ public class RequestHeaderServlet extends HttpServlet {
     }
 
 }
+
+/*
+    HTTP 요청 메세지를 통해 클라이언트에서 서버로 데이터를 전달하는 방법
+
+    1. GET - 쿼리 파라미터
+        - /url?username=hello&age=20
+        - 메시지 바디 없이 URL 의 쿼리 파라미터에서 데이터를 포함해서 전달
+        - 예) 검색 ,필터, 페이징 등에서 많이 사용하는 방식
+
+    2. POST 방식 - HTML Form
+        - content-type : application/w-xxx-form-urlencoded
+        - 메시지 바디에 쿼리 파라미터 형식으로 전달 username=hello&age=20
+        - 예) 회원가입, 사움 주문, HTML Form 사용
+
+    3. HTTP message body 에 데이터를 직접 담아서 요정
+        - HTTP API 에서 주로 사용, JSON, XML, TEXT
+        - 데이터 형식은 주로 JSON 사용
+        - POST, PUT, PATCH
+*/
