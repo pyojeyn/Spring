68dc8cf43c14bb7e7fd599349a70174c9261642d
pyojeyn <jeynpyo@gmail.com>
2022-04-17 15:43:42 +0900

    0417 뷰 리졸버

--- servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -7,11 +7,11 @@ import org.springframework.web.servlet.mvc.Controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@Component("/springmvc/old-controller")
+@Component("/springmvc/old-controller") // 스프링 빈의 이름 : /springmvc/old-controller -> url 패턴으로 맞춤.
 public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
-        return null;
+        return new ModelAndView("new-form"); // 논리적인 이름만.
     }
 }
